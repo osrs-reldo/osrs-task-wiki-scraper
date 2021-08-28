41771681475daaad1bb158ee4a89272782db81e2
Tyler Hardy <tylerthardy@gmail.com>
2021-08-28 13:10:27 -0400

    Move league parsing to util module

--- league1_tasks.py
+++ league1_tasks.py
@@ -3,6 +3,7 @@ import re
 import mwparserfromhell as mw
 import api
 import util
+import league_utils
 from typing import *
 
 
@@ -15,21 +16,7 @@ def run():
 		for section in code.get_sections():
 			sectionName = section.get(0)
 			for row in util.each_row("LeagueTaskRow", section):
-				skills = []
-				if row["s"] is not None:
-					skillCode = mw.parse(row["s"], skip_style_tags=True)
-					for skillPart in skillCode.filter_templates(matches=lambda t: t.name.matches("SCP")):
-						skills.append({
-							"skill": util.strip(str(skillPart.params[0])),
-							"level": util.strip(str(skillPart.params[1]))
-						})
-				task = {
-					"name": util.strip(str(row['1'])),
-					"description": util.strip(str(row['2'])),
-					"skills": skills,
-					"other": util.strip(str(row["other"])) if "other" in row else "",
-					"tier": util.strip(sectionName)
-				}
+				task = league_utils.convert_league_row_to_task(row, sectionName)
 				tasks.append(task)
 
 	except (KeyboardInterrupt, SystemExit):
---
+++ league_utils.py
@@ -0,0 +1,21 @@
+import mwparserfromhell as mw
+import util
+
+def convert_league_row_to_task(row, sectionName: str):
+	skills = []
+	if row["s"] is not None:
+		skillCode = mw.parse(row["s"], skip_style_tags=True)
+		for skillPart in skillCode.filter_templates(matches=lambda t: t.name.matches("SCP")):
+			skills.append({
+				"skill": util.strip(str(skillPart.params[0])),
+				"level": util.strip(str(skillPart.params[1]))
+			})
+	task = {
+		"name": util.strip(str(row['1'])),
+		"description": util.strip(str(row['2'])),
+		"skills": skills,
+		"other": util.strip(str(row["other"])) if "other" in row else "",
+		"tier": util.strip(sectionName)
+	}
+
+	return task
