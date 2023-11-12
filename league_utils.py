8a8eb41c9fe81e079397b526254828b83266ab28
chaiinchomp <chaiinchomp@gmail.com>
2023-11-12 14:07:40 -0800

    Fix out of bounds error

--- league_utils.py
+++ league_utils.py
@@ -6,13 +6,14 @@ def convert_league_row_to_task(row, sectionName: str):
 	if 's' in row and row["s"] is not None:
 		skillCode = mw.parse(row["s"], skip_style_tags=True)
 		for skillPart in skillCode.filter_templates(matches=lambda t: t.name.matches("SCP")):
-			skills.append({
-				"skill": util.strip(str(skillPart.params[0])),
-				"level": util.strip(str(skillPart.params[1]))
-			})
+			if (len(skillPart.params) > 1):
+				skills.append({
+					"skill": util.strip(str(skillPart.params[0])),
+					"level": util.strip(str(skillPart.params[1]))
+				})
 	task = {
 		"name": util.strip(str(row['1'])),
-		"description": util.strip(str(row['2'])),
+		"description": util.strip(str(row.get('2'))),
 		"skills": skills,
 		"other": util.strip(str(row["other"])) if "other" in row else "",
 		"tier": util.strip(sectionName)
